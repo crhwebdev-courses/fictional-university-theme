d192d946c97967c63df4dbd162bdb8fd63009d6a
Carl Hain <crhain@gmail.com>
2019-07-17 17:36:01 -0500

    Add side-bar menu to page.php that shows all current pages of the parent or the current page if it is a parent with links back to those pages

--- page.php
+++ page.php
@@ -34,13 +34,24 @@
      ?>
     
     
-    <!-- <div class="page-links">
+    <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php 
+         
+          if($theParent != 0){
+            $findChildrenOf = $theParent;
+          } else {
+            $findChildrenOf = get_the_ID();
+          }
+
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $findChildrenOf
+          ));
+        ?>
       </ul>
-    </div> -->
+    </div>
 
     <div class="generic-content">
       <?php the_content(); ?>

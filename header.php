4b4f4ce0f11a74b4a6eff6d86ceb0869de201857
Carl Hain <crhain@gmail.com>
2019-07-18 10:23:48 -0500

    Update header.php with meta tag to set content width to device-width for viewing on mobile devices

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 
 <html>
   <head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
   <!-- Load in stylesheet and other header stuff -->
   <?php wp_head(); ?>
   </head>

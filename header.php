2d7c9910b31d46cc43da910829fe59080bfca646
Carl Hain <crhain@gmail.com>
2019-07-18 10:28:00 -0500

    Update header.php to set character set automatically using a wordpress function

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <html <?php language_attributes() ?>>
   <head>
 
+  <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
   <!-- Load in stylesheet and other header stuff -->

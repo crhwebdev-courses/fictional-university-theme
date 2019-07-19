273ff9494a2424e6983e09f873e4a6454d279419
Carl Hain <crhain@gmail.com>
2019-07-19 16:56:07 -0500

    Comment out custom menu locations in header.php and footer.php, and revert back to hardcoded menus

--- footer.php
+++ footer.php
@@ -14,16 +14,19 @@
             <h3 class="headline headline--small">Explore</h3>
             <nav class="nav-list">
             <?php 
+            /*
+              //Adds custom menu to footer location
               wp_nav_menu(array(
                 'theme_location' => 'footerLocationOne'
               ));
+            */
             ?>
-              <!-- <ul>
+              <ul>
                 <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                 <li><a href="#">Programs</a></li>
                 <li><a href="#">Events</a></li>
                 <li><a href="#">Campuses</a></li>
-              </ul> -->
+              </ul>
             </nav>
           </div>
 
@@ -31,15 +34,18 @@
             <h3 class="headline headline--small">Learn</h3>
             <nav class="nav-list">
             <?php 
+            /*
+              //Adds custom menu to footer location
               wp_nav_menu(array(
                 'theme_location' => 'footerLocationTwo'
               ));
+            */
             ?>
-              <!-- <ul>
+              <ul>
                 <li><a href="#">Legal</a></li>
                 <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
                 <li><a href="#">Careers</a></li>
-              </ul> -->
+              </ul>
             </nav>
           </div>
         </div>

730d03935edba243aeb0643ca7b9c6367caeb305
Carl Hain <crhain@gmail.com>
2019-07-22 13:20:35 -0500

    Change metabox line for each post in index.php to include a dynamic link to author name, post date, and category

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
   <div class="post-item">
       <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <div class="metabox">
-        <p>Post by Carl on 6.8.19 in News</p>
+        <p>Post by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
       </div>
       <div class="generic-content">
       <?php the_excerpt(); ?>

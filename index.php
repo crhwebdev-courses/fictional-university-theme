9bf5c46a66eee0f07842d3a39427783426b4aadd
Carl Hain <crhain@gmail.com>
2019-07-21 21:58:01 -0500

    Add posts listing to index.php; each post shows a title with a link, an information line, and an excerpt

--- index.php
+++ index.php
@@ -12,6 +12,26 @@
   </div>  
 </div>
 
+<div class="container container--narrow page-section">
+  <?php 
+    while(have_posts()){
+      the_post();
+  ?>
+
+  <div class="post-item">
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <div class="metabox">
+        <p>Post by Carl on 6.8.19 in News</p>
+      </div>
+      <div class="generic-content">
+      <?php the_excerpt(); ?>
+      </div>
+  </div>
+
+  <?php    
+    }
+  ?>
+</div>
 
 <?php
   get_footer();

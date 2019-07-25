9092daf031544914420503c006c87af738aec22f
Carl Hain <crhain@gmail.com>
2019-07-24 19:07:20 -0500

    Add metabox section to single.php

--- single.php
+++ single.php
@@ -19,7 +19,16 @@
 
 
 <div class="container container--narrow page-section">
-    metabox will go here
+    
+    <div class="metabox metabox--position-up metabox--with-home-link">
+      <p>
+        <a class="metabox__blog-home-link" href="<?php echo site_url('/blog') ?>">
+          <i class="fa fa-home" aria-hidden="true"></i> Blog Home 
+        </a> 
+        <span class="metabox__main"><?php the_title(); ?></span>
+      </p>
+    </div>
+
     <div class="generic-content">
       <?php 
         the_content();
